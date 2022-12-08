47f130c84bd7655c3f4933a7ca2aa3e61f08f917
Mauricio <maurolores1992@gmail.com>
2022-12-08 16:16:15 -0300

    php

--- pages/enviarmensaje.php
+++ pages/enviarmensaje.php
@@ -1,15 +1,15 @@
 <?php
+
 $destino = "maurolores1992@gmail.com";
 $nombre = $_POST ['nombre'];
 $celular = $_POST ['celular'];
 $email = $_POST ['email'];
 $mensaje = $_POST ['mensaje'];
 
-$header = "Content-Type: text/plain"
+$header = "Envio desde formulario de contacto en localhost";
 $mensajeCompleto = "\n Nombre : " . $nombre . "\n" . "Email: " . $email . "\n" . "Numero de telefono: " . $celular . "\n" . "Mensaje : " . $mensaje;
 
 mail($destino, $mensajeCompleto, $header);
-
 header('Location: exito.html');
 
 ?>
