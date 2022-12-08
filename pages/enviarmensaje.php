2b950cc09c441e8e25334e25bef8516f4758831f
Mauricio <maurolores1992@gmail.com>
2022-12-08 16:20:05 -0300

    terminando php

--- pages/enviarmensaje.php
+++ pages/enviarmensaje.php
@@ -1,15 +1,33 @@
 <?php
 
-$destino = "maurolores1992@gmail.com";
-$nombre = $_POST ['nombre'];
-$celular = $_POST ['celular'];
+// Conecto mi form / name / con el server a traves de los name
+
+$nombre = $_POST ['name'];
+$celular = $_POST ['celular']; 
 $email = $_POST ['email'];
-$mensaje = $_POST ['mensaje'];
+$textarea = $_POST ['mensaje'];
 
-$header = "Envio desde formulario de contacto en localhost";
-$mensajeCompleto = "\n Nombre : " . $nombre . "\n" . "Email: " . $email . "\n" . "Numero de telefono: " . $celular . "\n" . "Mensaje : " . $mensaje;
+// Vamos a mostrar un texto plano
+$header .= "Content-Type: text/plain";
 
-mail($destino, $mensajeCompleto, $header);
+// Como me va a llegar el cierpo del mail a mi, osea lo que la gente escribio en el form - /r/n es para hacer saltos de linea
+
+$mensaje = "Este mensaje fue enviado por:" . $nombre . ",/r/n";
+$mensaje = "Su numero de telefono es:" . $celular . ",/r/n";
+$mensaje = "Su email es:" . $email . ",/r/n";
+$mensaje = "Mensaje enviado" . $textarea . ",/r/n";
+$mensaje = "mensaje" . $_POST ["mensaje"];
+$mensaje = "Enviado el" . date ("d/m/Y", time ());
+
+// Funcion mail 
+// a quien le mando el mail 
+
+$para = 'maurolores1992@gmail.com';
+$asunto = 'Mensaje de Fix it PC';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+// redireccion al haber enviado el mail 
 header('Location: exito.html');
 
 ?>
