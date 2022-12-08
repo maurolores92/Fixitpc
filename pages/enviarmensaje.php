541ca9a9d04dbc49c9dbd2884dd6121325f0f01d
Mauricio <maurolores1992@gmail.com>
2022-12-08 16:40:27 -0300

    php

--- pages/enviarmensaje.php
+++ pages/enviarmensaje.php
@@ -6,10 +6,11 @@ $asunto = 'Mensaje de Fix it PC';
 $nombre = $_POST ['nombre'];
 $celular = $_POST ['celular'];
 $email = $_POST ['email'];
+$asunto = $_POST ['asunto'];
 $textarea = $_POST ['textarea'];
 
 $header = "Envio desde formulario de contacto de Fix it PC";
-$mensaje = "Nombre: " . $nombre . ",/r/n" . "Email: " . $email . ",/r/n" . "Numero de telefono: " . $celular . ",/r/n" . "Mensaje : " . $textarea;
+$mensaje = "Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Numero de telefono: " . $celular . "\n" . "Servicio solicitad : " . $asunto . "\n" . "Mensaje : " . $textarea;
 
 mail($destino, $header, utf8_decode($mensaje));
 header('Location: exito.html');
