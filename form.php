cdfec8c10353cf9243dfbf6937104f25c0e182f8
Mauricio <maurolores1992@gmail.com>
2022-12-08 14:48:23 -0300

    agregando php

---
+++ form.php
@@ -0,0 +1,30 @@
+<?php
+
+// Conecto mi form / name / con el server a traves de los name
+
+$nombre = $_POST ["name"];
+$celular = $_POST ["celular"]; 
+$email = $_POST ["email"];
+$mensaje = $_POST ["textarea"];
+
+// Como me va a llegar el cierpo del mail a mi, osea lo que la gente escribio en el form - /r/n es para hacer saltos de linea
+
+$mensaje = "Este mensaje fue enviado por:" . $nombre . ",/r/n";
+$mensaje = "Su numero de telefono es:" . $celular . ",/r/n";
+$mensaje = "Su email es:" . $email . ",/r/n;"
+$mensaje = "mensaje" . $_POST ["mensaje"]
+$mensaje = "Enviado el" . date ("d/m/Y", time ())
+
+// Funcion mail 
+// a quien le mando el mail 
+
+$para = "mauriciolores1992@gmail.com";
+$asunto = "Mensaje de Fix it PC"
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+// redireccion al haber enviado el mail
+
+header("Location:pages/exito.html")
+
+?>
